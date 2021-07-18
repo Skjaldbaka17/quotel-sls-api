3aeaaefed76c57eb689201ec4f5876cbe808e2ae
skjaldbaka17 <skjaldbaka17@gmail.com>
2021-07-18 12:13:49 +0000

    Adapted search-by-string route + added tests

--- functions/search/search-by-string/main.go
+++ functions/search/search-by-string/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type RequestHandler struct {
@@ -18,24 +19,49 @@ type RequestHandler struct {
 
 var theReqHandler = RequestHandler{}
 
-func search(requestBody *structs.Request, dbPointer *gorm.DB) *gorm.DB {
-	table := "searchview"
+func search(requestBody *structs.Request, dbPointer *gorm.DB, searchString string) *gorm.DB {
+	table := "quotes"
 	//TODO: Validate that this topicId exists
-	if requestBody.TopicId > 0 {
+	if len(requestBody.TopicIds) > 0 {
 		table = "topicsview"
 	}
 	dbPointer = dbPointer.Table(table+", plainto_tsquery('english', ?) as plainq ",
-		requestBody.SearchString).Select("*, ts_rank(tsv, plainq) as plainrank")
+		searchString).Select("*, ts_rank(tsv, plainq) as plainrank")
 
-	if requestBody.TopicId > 0 {
-		dbPointer = dbPointer.Where("topic_id = ?", requestBody.TopicId)
+	if len(requestBody.TopicIds) > 0 {
+		dbPointer = dbPointer.Where("topic_id in ?", requestBody.TopicIds)
 	}
 
 	//Order by authorid to have definitive order (when for examplke some quotes rank the same for plain, phrase, general and similarity)
-	dbPointer = dbPointer.Where("( tsv @@ plainq )").Order("plainrank desc, author_id desc")
+	dbPointer = dbPointer.Where("( tsv @@ plainq )").Order("plainrank desc, id desc")
 
 	//Particular language search
 	dbPointer = utils.QuoteLanguageSQL(requestBody.Language, dbPointer)
+	return dbPointer
+}
+
+func authorSearch(requestBody *structs.Request, dbPointer *gorm.DB, searchString string) *gorm.DB {
+	table := "quotes"
+	//TODO: Validate that this topicId exists
+	if len(requestBody.TopicIds) > 0 {
+		table = "topicsview"
+	}
+	dbPointer = dbPointer.Table(table).
+		Where("( similarity(name, ?) > 0.4)", requestBody.SearchString)
+
+	dbPointer = utils.AuthorLanguageSQL(requestBody.Language, dbPointer)
+
+	if len(requestBody.TopicIds) > 0 {
+		dbPointer = dbPointer.Where("topic_id in ?", requestBody.TopicIds)
+	}
+
+	//Particular language search
+	dbPointer = utils.QuoteLanguageSQL(requestBody.Language, dbPointer)
+
+	dbPointer = dbPointer.
+		Clauses(clause.OrderBy{
+			Expression: clause.Expr{SQL: "similarity(name,?) desc, id desc", Vars: []interface{}{requestBody.SearchString}, WithoutParentheses: true},
+		})
 	return dbPointer
 }
 
@@ -66,15 +92,18 @@ func (requestHandler *RequestHandler) handler(request events.APIGatewayProxyRequ
 		}, nil
 	}
 
-	var topicResults []structs.TopicViewDBModel
+	var topicResults []structs.QuoteDBModel
 
 	var dbPointer *gorm.DB
-	for i := 0; i < 2; i++ {
-		if i == 1 {
-			requestBody.SearchString = requestHandler.CheckForSpellingErrorsInSearchString(requestBody.SearchString, "unique_lexeme")
+	for i := 0; i < 3; i++ {
+		if i == 0 {
+			dbPointer = search(&requestBody, requestHandler.Db, requestBody.SearchString)
+		} else if i == 1 {
+			searchString := requestHandler.CheckForSpellingErrorsInSearchString(requestBody.SearchString, "unique_lexeme")
+			dbPointer = search(&requestBody, requestHandler.Db, searchString)
+		} else if i == 2 {
+			dbPointer = authorSearch(&requestBody, requestHandler.Db, requestBody.SearchString)
 		}
-
-		dbPointer = search(&requestBody, requestHandler.Db)
 
 		err := utils.Pagination(requestBody, dbPointer).
 			Find(&topicResults).Error
@@ -89,15 +118,25 @@ func (requestHandler *RequestHandler) handler(request events.APIGatewayProxyRequ
 				StatusCode: http.StatusInternalServerError,
 			}, nil
 		}
-
 		if len(topicResults) > 0 {
 			break
 		}
 	}
 
+	if len(topicResults) == 0 {
+		errResponse := structs.ErrorResponse{
+			Message:    "There are no quotes matching your search. Please check your string for spelling errors etc.",
+			StatusCode: http.StatusOK,
+		}
+		return events.APIGatewayProxyResponse{
+			Body:       errResponse.ToString(),
+			StatusCode: http.StatusOK,
+		}, nil
+
+	}
 	//Update popularity in background! TODO: Add as its own lambda function
 	go requestHandler.TopicViewAppearInSearchCountIncrement(topicResults)
-	apiResults := structs.ConvertToTopicViewsAPIModel(topicResults)
+	apiResults := structs.ConvertToQuotesAPIModel(topicResults)
 	out, _ := json.Marshal(apiResults)
 	return events.APIGatewayProxyResponse{
 		Body:       string(out),
